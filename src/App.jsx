25e1592b8789350e49c1e75ae2149d6d384dcaf9
ahmadibni <ahmadibni007@gmail.com>
2024-12-12 14:57:26 +0700

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import { useRef, useState, useCallback, useEffect } from "react";
+
+import Places from "./components/Places.jsx";
+import Error from "./components/Error.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import { fetchUserPlaces, updateUserPlaces } from "./http.js";
+
+function App() {
+  const selectedPlace = useRef();
+
+  const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [errorFetchingState, setErrorFetchingState] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const places = await fetchUserPlaces();
+
+        setUserPlaces(places);
+      } catch (error) {
+        setErrorFetchingState({
+          message: error.message || "Failed deleting places",
+        });
+      }
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  function handleStartRemovePlace(place) {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
+
+  async function handleSelectPlace(selectedPlace) {
+    setUserPlaces((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
+
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorFetchingState({
+        message: error.message || "Failed updating places",
+      });
+    }
+  }
+
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id
+        )
+      );
+
+      try {
+        await updateUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+        );
+      } catch (error) {
+        setUserPlaces(userPlaces);
+        setErrorFetchingState({
+          message: error.message || "Failed deleting places",
+        });
+      }
+
+      setModalIsOpen(false);
+    },
+    [userPlaces]
+  );
+
+  function handleError() {
+    setErrorFetchingState(null);
+  }
+
+  return (
+    <>
+      <Modal open={errorFetchingState} onClose={handleError}>
+        {errorFetchingState && (
+          <Error
+            title="An error occured!"
+            message={errorFetchingState.message}
+            onConfirm={handleError}
+          />
+        )}
+      </Modal>
+
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          loadingText="Loading user places. . ."
+          isLoading={isFetching}
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+      </main>
+    </>
+  );
+}
+
+export default App;
